6b86a24831cb17aceccb49f80f373e7b76765a9b
Anthony Dombrowski <anthonydombrowski@pingidentity.com>
2023-03-16 14:31:31 -0500

    yarn didn't work. using npm

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import "./App.css";
 import { useFetch } from "react-async";
 
 // To be replaced by the endpoint of the API deployed through the CloudFormation Template
-const APIEndPoint = "to be replaced with your api endpoint here";
+const APIEndPoint = "https://adrqd0vz77.execute-api.us-east-1.amazonaws.com/v1/hello";
 
 function App() {
   const getURLInfo = () => {
